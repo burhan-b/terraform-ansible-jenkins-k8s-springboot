946e20146453e40200fd56708411fa62ae0d9404
burhan-b <92991050+burhan-b@users.noreply.github.com>
2022-05-07 16:07:58 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-1"
-  access_key = "AKIA2LB7BFNGDPRW4S4X"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
